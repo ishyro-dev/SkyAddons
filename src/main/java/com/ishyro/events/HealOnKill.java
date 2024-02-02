16180f31135b7978cabb4730bfbc2d24ed8ea09b
ishyro-dev <gamere06@gmail.com>
2024-02-02 11:29:20 -0400

    Fix NullPointerException

--- src/main/java/com/ishyro/events/HealOnKill.java
+++ src/main/java/com/ishyro/events/HealOnKill.java
@@ -1,16 +1,17 @@
 package com.ishyro.events;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class HealOnKill implements Listener {
-    @EventHandler
-    public void entityDeath(EntityDeathEvent e) {
-        if (e.getEntity() != null && e.getEntity() instanceof Player) {
-            Player k = e.getEntity().getKiller();
-            assert k != null;
-            k.setHealth(20);
+    @EventHandler(priority= EventPriority.MONITOR, ignoreCancelled=true)
+    public void onPlayerDeath(PlayerDeathEvent event){
+        Player killer = event.getEntity().getKiller();
+        if ( (killer != null)){
+            killer.setHealth(20);
         }
     }
-    }
+}
+
